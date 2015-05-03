fbcb068aad4e8d17a343ce5137ba7aa23ae170d2
MOKU SACHINDAR REDDY <Sachindarsharad.moku@stud.hochschule-heidelberg.de>
2015-05-03 14:45:33 +0300

    Revert "modified iradioui"
    This reverts commit 92f1d366a53c6c619b54b07af22bf6e143ae5beb.

--- src/com/example/iradio/IradioUI.java
+++ src/com/example/iradio/IradioUI.java
@@ -3,9 +3,13 @@ package com.example.iradio;
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
